5d756cf73b420000938aee83f79d14500433fd67
Gavin Stark <g.stark@realdigitalmedia.com>
2008-10-10 08:35:40 -0400

    Adding ability to determine current and next talks.

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -4,10 +4,14 @@ class Talk < ActiveRecord::Base
   named_scope :by_time,   :order => :start_time
   named_scope :morning,   :conditions => [ "time(start_time) <  '12:00' " ]
   named_scope :afternoon, :conditions => [ "time(start_time) >= '12:00' " ]
-  
+
+  # Find the current and next talk
+  named_scope :active,    lambda { |*args| named_scope_active( *args ) }
+  named_scope :next,      lambda { |*args| named_scope_next( *args ) }
+
   validates_presence_of :name, :room_id, :start_time, :end_time
   validate :timecheck
-  
+
   def timecheck
     min = Date.new(2000,1,1)
     if start_time && end_time
@@ -15,5 +19,15 @@ class Talk < ActiveRecord::Base
       errors.add_to_base("Please set end time after start time") if end_time <= start_time
     end
   end
-   
+
+  private
+    def self.named_scope_active( *args )
+      time = args.first || Time.now
+      { :conditions => [ "time(start_time) <= time(?) and time(end_time) >= time(?)", time, time ] }
+    end
+    
+    def self.named_scope_next( *args )
+      time = args.first || Time.now
+      { :conditions => [ "time(start_time) >= time(?)", time ], :order => "start_time", :limit => 1 }
+    end
 end
