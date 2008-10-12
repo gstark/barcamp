333740781be205a93372cf4815dedec66230ea74
Gavin Stark <g.stark@realdigitalmedia.com>
2008-10-12 06:49:47 -0400

    Reformatting homepage to put alternative links at the TOP and move sponsors to the bottom.  Based on feedback from mobile phone users.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
         current_user.remember_me unless current_user.remember_token?
         cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
       end
-      redirect_back_or_default('/')
+      redirect_back_or_default( root_path )
       flash[:notice] = "Welcome back #{current_user.login}"
     else
       flash[:notice] = "Please try again"
